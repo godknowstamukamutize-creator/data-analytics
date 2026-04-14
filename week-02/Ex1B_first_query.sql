9514f4bf00894dd4aeca6a0d708ce226438ddb55
Godknows Mutize <godknowstamukamutize@icloud.com>
2026-04-14 11:07:05 -0500

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,10 @@
 /*
-This database is used to collect information about our company employees  for the use by management
+This database is used to collect information about Financial Company, including demographic information for the use by management
 /*
+
+
+SELECT* 
+FROM actor; -- Retrieved 200 records
+
+SELECT * 
+FROM film; -- Retrieved 1000 records
