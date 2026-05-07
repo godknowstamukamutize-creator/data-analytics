1e869e8f839ef67179fdadb508e01f26c51e79db
Godknows Mutize <godknowstamukamutize@icloud.com>
2026-05-07 14:35:59 -0500

    "Networth"

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,17 @@
+# Assets
+checking_account = 4500.00
+savings_account = 10000.00
+car_value = 25000.00
+total_assets = checking_account + savings_account + car_value
+
+# Debts
+student_loan = 25000.00
+car_loan = 5000.00
+credit_card = 2000.00
+total_debts = student_loan + car_loan + credit_card
+
+net_worth = total_assets - total_debts
+
+print("Your total assets are " + str(total_assets))
+print("Your total debts are " + str(total_debts))
+print("Your net worth is " + str(net_worth))
