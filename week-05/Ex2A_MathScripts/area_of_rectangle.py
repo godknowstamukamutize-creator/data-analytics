c3c85e0934f9b96bde3e742e45e5c1c819917611
Godknows Mutize <godknowstamukamutize@icloud.com>
2026-05-07 14:40:42 -0500

    "Area_Calc

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,7 @@
+side_a = 1   # birth month
+side_b = 1  # birth day
+area = side_a * side_b
+
+print("Side A is " + str(side_a))
+print("Side B is " + str(side_b))
+print("The area of the rectangle is " + str(area))
