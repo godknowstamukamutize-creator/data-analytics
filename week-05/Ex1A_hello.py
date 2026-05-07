9a14e3b6eea34649651ebcae6a6ddea519ac328d
Godknows Mutize <godknowstamukamutize@icloud.com>
2026-05-07 14:04:52 -0500

    "Ex1B_variables"

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,23 @@
 print('Hello world!')
+
+message = 'Hello world!'
+print(message)
+# "Hello world" prints twice because we have two print statements
+
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+
+# Exercise 19
+# The result is 3.5 — Python adds the numbers together as floats.
+# dollars (3) + cents (0.50) = 3.5
+cents = cents + .25
+print(dollars + cents)
+
+
+# Exercise 21
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
