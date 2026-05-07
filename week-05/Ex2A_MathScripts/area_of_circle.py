06659060feac5d1850c19f507fe5c31be0f5fad3
Godknows Mutize <godknowstamukamutize@icloud.com>
2026-05-07 14:46:34 -0500

    "Area_of_a_Circle"

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,7 @@
+import math
+
+diameter = 1  # birthday day
+radius = diameter / 2
+area = math.pi * radius ** 2
+
+print("The area of a circle with radius " + str(radius) + " is " + format(area, ".2f"))
